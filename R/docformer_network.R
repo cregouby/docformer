c877366fbe5931b428531da0d6165475887246ab
Christophe Regouby <christophe.regouby@free.fr>
2021-12-21 11:01:24 +0100

    first package boilerplate

---
+++ R/docformer_network.R
@@ -0,0 +1,141 @@
+positional_encoding <- torch::nn_module(
+  "positional_encoding",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+resnet_feature_extractor <- torch::nn_module(
+  "resnet_feature_extractor",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+docformer_embeddings <- torch::nn_module(
+  "docformer_embeddings",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+pre_norm <- torch::nn_module(
+  "pre_norm",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+pre_norm_attention <- torch::nn_module(
+  "pre_norm_attention",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+feed_forward <- torch::nn_module(
+  "feed_forward",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+relative_position <- torch::nn_module(
+  "relative_position",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+
+multimodal_attention_layer <- torch::nn_module(
+  "multimodal_attention_layer",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+docformer_encoder <- torch::nn_module(
+  "docformer_encoder",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+
+language_feature_extractor <- torch::nn_module(
+  "language_feature_extractor",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+extract_features <- torch::nn_module(
+  "extract_features",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+
+docformer_for_classification <- torch::nn_module(
+  "docformer_for_classification",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+
+docformer_for_ir <- torch::nn_module(
+  "docformer_for_ir",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+
+docformer <- torch::nn_module(
+  "docformer",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
+
+shallow_decoder <- torch::nn_module(
+  "shallow_decoder",
+  initialize = function(){
+
+  },
+  forward = function() {
+
+  }
+)
