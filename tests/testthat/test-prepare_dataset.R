8e2e2a233bcbd936bc182c7d14e3bc2b207242f3
Christophe Regouby <christophe.regouby@free.fr>
2022-01-03 08:50:55 +0100

    remove unused test

--- tests/testthat/test-prepare_dataset.R
+++ tests/testthat/test-prepare_dataset.R
@@ -2,7 +2,7 @@ test_that("normalize_box works with single var", {
   expect_equal(normalize_box(c(22,34,27,41), width=100, height=100, size=100), c(22,34,27,41))
 })
 
-test_that("normalize_box works with dataframe", {
-  df <- data.frame(bbox=c(c(22,34,27,41),c(22,34,27,41)),  width=c(100,100), height=c(100,100), size=c(100,100))
-  expect_equal(all(normalize_box(df), c(22,34,27,41)))
-})
+# test_that("normalize_box works with dataframe", {
+#   df <- tibble::tibble(bbox=c(list(22,34,27,41),list(22,34,27,41)),  width=c(100,100), height=c(100,100), size=c(100,100))
+#   expect_equal(all(normalize_box(df), c(22,34,27,41)))
+# })
