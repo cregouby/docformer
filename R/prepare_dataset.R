58b3f1c2754674a5a7b44ca6227a02437b23bee5
Christophe Regouby <christophe.regouby@free.fr>
2022-01-24 09:05:23 +0100

    make step 4 work with hftokenizer. fix errors in step 1 and step 3

--- R/prepare_dataset.R
+++ R/prepare_dataset.R
@@ -43,9 +43,9 @@ resize_align_bbox <- function(bbox, origin_w, origin_h, target_w, target_h) {
 #'    apply_ocr()
 apply_ocr <- function(image) {
   ocr_df <- tesseract::ocr_data(image) %>%
-    mutate(poor_word =( str_detect(word, "^\\W+$|\\W{3,}") | confidence <20),
-           bb = bbox %>% str_split(",")) %>%
-    unnest_wider(bb, names_sep="_") %>%
+    mutate(poor_word =( stringr::str_detect(word, "^\\W+$|\\W{3,}") | confidence <20),
+           bb = bbox %>% stringr::str_split(",") %>% purrr::map(~readr::parse_number(.x))) %>%
+    tidyr::unnest_wider(bb, names_sep="_") %>%
     filter(!poor_word) %>%
     select(word, confidence, xmin=bb_1, ymin=bb_2, xmax=bb_3, ymax=bb_4)
   return(ocr_df)
@@ -54,7 +54,8 @@ apply_ocr <- function(image) {
 #' Turn image into docformer torch tensor input feature
 #'
 #' @param image file path, url, or raw vector to image (png, tiff, jpeg, etc)
-#' @param tokenizer tokenizer function to apply to words extracted from image
+#' @param tokenizer tokenizer function to apply to words extracted from image. Currently, only
+#'   hftokenizer tokenizer is supported.
 #' @param add_batch_dim (boolean) add a extra dimension to tensor for batch encoding
 #' @param target_geometry image target magik geometry expected by the image model input
 #' @param max_seq_len size of the embedding vector in tokens
@@ -79,25 +80,32 @@ create_features <- function(image,
 
   # step 1 read images and its attributes
   original_image <- image_read(image)
-  w_h <- image_attributes(original_image)
+  w_h <- image_info(original_image)
 
   # step 2: resize image
   resized_image <- image_resize(original_image, geometry=target_geometry)
 
   # step 3 extract text throuhg OCR and normalize bbox to 1000x1000
   entries <- apply_ocr(original_image) %>%
-    mutate(xmin = round(xmin/width * 1000),
-           ymin= round(ymin/height * 1000),
-           xmax = round(xmax/width * 1000),
-           ymax= round(ymax/height * 1000)
+    mutate(xmin = round(xmin/w_h$width * 1000),
+           ymin= round(ymin/w_h$height * 1000),
+           xmax = round(xmax/w_h$width * 1000),
+           ymax= round(ymax/w_h$height * 1000)
     )
 
   # step 4 tokenize words and get their bbox
-  encoding <- tokenizer(entities$word,
-                        padding = "max_length",
-                        max_length = max_seq_length,
-                        is_split_into_words = TRUE,
-                        truncation = TRUE,
-                        add_special_tokens = FALSE)
-  token_bbox <-
+  ## case hftokenizer
+  if (inherits(tokenizer, c("tokenizer", "R6"))) {
+    entries <- entries %>%
+      mutate(idx = map(entries$word, ~tokenizer$encode(.x)$ids))
+  } else {
+    rlang::abort(paste0(tokenizer," is not recognized as a supported tokenizer"))
+  }
+
+  # token_boxes, unnormalized_token_boxes = get_tokens_with_boxes(unnormalized_word_boxes,
+  #                                                               normalized_word_boxes,
+  #                                                               PAD_TOKEN_BOX,
+  #                                                               encoding.word_ids())
+  #
+  token_bbox
 }
