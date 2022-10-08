db086714c2b1d00638f3dcc309073d7fd9b0bfeb
C. Regouby <christophe.regouby@free.fr>
2022-10-08 19:29:44 +0200

    move masked_lm code in its own file working ltr with Resnet50 VAE instead of VDSR ══ Testing test-docformer_mlm.R ════════════════════════════════════════════════ [ FAIL 0 | WARN 0 | SKIP 0 | PASS 13 ] Done!
    Test complete

---
+++ R/docformer_mlm.R
@@ -0,0 +1,128 @@
+
+# vdsr reconstruction as a port of https://github.com/twtygqyy/pytorch-vdsr/blob/master/vdsr.py
+ltr_conv_relu_block <- torch::nn_module(
+  "ltr_conv_relu_block",
+  initialize = function() {
+    self$conv <- torch::nn_conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 1, bias = FALSE)
+    torch::nn_init_normal_(self$conv$weight, 0, sqrt(2 / (3 * 3 * 64)))
+    self$relu <- torch::nn_relu(inplace = TRUE)
+
+  },
+  forward = function(x) {
+    self$relu(self$conv(x))
+  }
+)
+
+# from ResNet VAE Decoder https://github.com/hsinyilin19/ResNetVAE/blob/master/modules.py
+ltr_head <- torch::nn_module(
+  "ltr_head",
+  initialize = function() {
+    # CNN architechtures
+    self$k1 <- c(5, 5)      # 2d kernal size
+    self$k2 <- self$k3 <- self$k4 <- c(3, 3)      # 2d kernal size
+    self$s1 <-  self$s2 <- self$s3 <- self$s4 <- c(2, 2)      # 2d strides
+    self$pd1 <- self$pd2 <- self$pd3 <-  self$pd4 <- c(0, 0)  # 2d padding
+
+    # Sampling vector : a nn_linear layer from x$shape[3] to 768 would be meaningfull
+    # Decoder
+    self$convTrans6 <- torch::nn_sequential(
+      torch::nn_conv_transpose2d(in_channels = 512, out_channels = 128, kernel_size = self$k4,
+                                 stride = self$s4, padding = self$pd4),
+      torch::nn_batch_norm2d(128, momentum = 0.01),
+      torch::nn_relu(inplace = TRUE),
+    )
+    self$convTrans7 <- torch::nn_sequential(
+      torch::nn_conv_transpose2d(in_channels = 128, out_channels = 32, kernel_size = self$k3,
+                                stride = self$s3, padding = self$pd3),
+      torch::nn_batch_norm2d(32, momentum = 0.01),
+      torch::nn_relu(inplace = TRUE),
+    )
+    self$convTrans8 <- torch::nn_sequential(
+      torch::nn_conv_transpose2d(in_channels = 32, out_channels = 8, kernel_size = self$k2,
+                                stride = self$s2, padding = self$pd2),
+      torch::nn_batch_norm2d(8, momentum = 0.01),
+      torch::nn_relu(inplace = TRUE),
+    )
+
+    self$convTrans9 <- torch::nn_sequential(
+      torch::nn_conv_transpose2d(in_channels = 8, out_channels = 3, kernel_size = self$k1,
+                                stride = self$s1, padding = self$pd1),
+      torch::nn_batch_norm2d(3, momentum = 0.01),
+      torch::nn_sigmoid()    # y <- (y1, y2, y3) \in [0 ,1]^3
+    )
+
+    self$relu <- torch::nn_relu(inplace = TRUE)
+
+  },
+  forward = function(x) {
+    img_reconstruct <- x$reshape(c(x$shape[1:2], 32, 24 ))
+    img_reconstruct <-  self$convTrans6(img_reconstruct)
+    img_reconstruct <-  self$convTrans7(img_reconstruct)
+    img_reconstruct <-  self$convTrans8(img_reconstruct)
+    img_reconstruct <-  self$convTrans9(img_reconstruct)
+    img_reconstruct
+  }
+)
+
+tdi_head <- torch::nn_module(
+  "tdi_head",
+  initialize = function(config) {
+    # TODO
+
+  },
+  forward = function(x) {
+    # TODO
+    x
+  }
+)
+
+docformer_for_masked_lm <- torch::nn_module(
+  "docformer_for_masked_LM",
+  initialize = function(config, mask_id) {
+    self$config <- config
+    self$mask_id <- mask_id
+    self$docformer <- docformer(config)
+
+    self$mm_mlm <- LayoutLMLMPredictionHead(config)
+    self$ltr <- ltr_head()
+    self$tdi <- tdi_head(config)
+
+    self$mlm_loss <- torch::nn_cross_entropy_loss()
+    self$ltr_loss <- torch::nn_smooth_l1_loss()
+    self$tdi_loss <- torch::nn_bce_with_logits_loss()
+  },
+  forward = function(x) {
+    # compute sequence embedding
+    embedding <- self$docformer(x)
+    # compute Multi-Modal Masked Language Modeling (MM-MLM) and loss
+    masked_embedding <- self$docformer(mask_for_tdi(mask_for_ltr(mask_for_mm_mlm(x, self$mask_id))))
+    mm_mlm <- self$mm_mlm(masked_embedding)
+    long_shape <- x$text$shape[1] * self$config$max_position_embeddings
+    mm_mlm_loss <- self$mlm_loss(
+      mm_mlm$view(c(-1, config$vocab_size)),
+      (x$text + 1L)$view(long_shape))$to(torch::torch_long())
+    #  compute Learn To Reconstruct (LTR) the image and loss
+    ltr <- self$ltr(masked_embedding)
+    ltr_loss <- self$ltr_loss(torch::nnf_interpolate(ltr, x$image$shape[3:4]), x$image)
+    # TODO compute Text Describes Image (TDI) loss
+    tdi <- self$tdi(masked_embedding)
+    # compute loss
+    masked_lm_loss <- (
+      5 * mm_mlm_loss +
+        ltr_loss +
+        5 * self$tdi_loss(tdi, x)
+    )
+
+    # TODO BUG compute logits
+    # prediction_scores <- mm_mlm
+
+    # TODO extract other piggyback values see layoutlm_network.R @826
+    result <- list(
+      loss = masked_lm_loss,
+      hidden_states = embedding$hidden_states,
+      attentions = embedding$attentions
+    )
+    class(result) <- "MaskedLMOutput"
+    return(result)
+  }
+)
--- tests/testthat/test-docformer_mlm.R
+++ tests/testthat/test-docformer_mlm.R
@@ -7,10 +7,10 @@ test_that("docformer_for_masked_lm unitary functions work", {
   # mm_mlm
   expect_no_error(mm_mlm <- docformer_net$mm_mlm(embedding))
   expect_tensor_shape(mm_mlm, c(2, config$max_position_embeddings, config$vocab_size))
-  expect_tensor_dtype(mm_mlm, torch::torch_float16())
+  expect_tensor_dtype(mm_mlm, "Float")
   # ltr
-  expect_no_error(ltr <- docformer_net$ltr(embedding[,1,]))
-  expect_tensor_shape(ltr, c(2, config$max_position_embeddings, config$hidden_size))
+  expect_no_error(ltr <- docformer_net$ltr(embedding))
+  expect_no_error(ltr_loss <- docformer_net$ltr_loss(torch::nnf_interpolate(ltr, doc_tt$image$shape[3:4]), doc_tt$image))
   # tdi
   expect_no_error(tdi <- docformer_net$tdi(embedding))
   expect_tensor_shape(tdi, c(2, config$max_position_embeddings, config$hidden_size))
